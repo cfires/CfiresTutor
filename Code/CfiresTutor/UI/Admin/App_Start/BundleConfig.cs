bdc542ac61ade57a2c00e746ea9954598acef417
cfires <caofangzh@163.com>
2016-10-21 17:39:06 +0800

    init admin

--- Code/CfiresTutor/UI/Admin/App_Start/BundleConfig.cs
+++ Code/CfiresTutor/UI/Admin/App_Start/BundleConfig.cs
@@ -9,23 +9,23 @@ namespace CfiresTutor.UI.Admin
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                      "~/Content/Scripts/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/Scripts/jquery/jquery.validate*"));
 
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/Scripts/bootstrap/bootstrap.js",
+                      "~/Content/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Scripts/bootstrap/bootstrap.css",
+                      "~/Content/Styles/site.css"));
         }
     }
 }
